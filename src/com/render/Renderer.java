cd2bfbe57d32eb08f95a69c51e90560f4cc8e67b
Vitaly Maisevich <satanpriest666@mail.ru>
2024-03-30 02:12:30 +0300

    Разбил текстурирование на два парралельных потока в тестовых целях

--- src/com/render/Renderer.java
+++ src/com/render/Renderer.java
@@ -7,6 +7,8 @@ import java.awt.event.WindowEvent;
 import java.awt.geom.Path2D;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Renderer {
 
@@ -134,21 +136,45 @@ public class Renderer {
 
                     double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
 
-                    for (int y = minY; y <= maxY; y++) {
-                        for (int x = minX; x <= maxX; x++) {
-                            double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
-                            double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
-                            double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
-                            if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
-                                int zIndex = y * img.getWidth() + x;
-                                if (zBuffer[zIndex] < depth) {
-                                    img.setRGB(x, y, Shades.getShade(t.color, angleCos).getRGB());
-                                    zBuffer[zIndex] = depth;
+
+                    ExecutorService executor = Executors.newFixedThreadPool(2);
+                executor.submit(() -> {
+                            for (int y = minY; y <= maxY; y += 2) {
+                                for (int x = minX; x <= maxX; x++) {
+                                    double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                                    double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                                    double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                                    if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                        double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                        int zIndex = y * img.getWidth() + x;
+                                        if (zBuffer[zIndex] < depth) {
+                                            img.setRGB(x, y, Shades.getShade(t.color, angleCos).getRGB());
+                                            zBuffer[zIndex] = depth;
+                                        }
+                                    }
                                 }
                             }
-                        }
-                    }
+                    });
+
+                    executor.submit(() -> {
+                            for (int y = minY+1; y <= maxY; y += 2) {
+                                for (int x = minX; x <= maxX; x++) {
+                                    double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                                    double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                                    double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                                    if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
+                                        double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                        int zIndex = y * img.getWidth() + x;
+                                        if (zBuffer[zIndex] < depth) {
+                                            img.setRGB(x, y, Shades.getShade(t.color, angleCos).getRGB());
+                                            zBuffer[zIndex] = depth;
+                                        }
+                                    }
+                                }
+                            }
+                    });
+                    executor.shutdown();
+
                     g2.drawImage(img, -getWidth() / 2, -getHeight() / 2, null);
                 }
             }
